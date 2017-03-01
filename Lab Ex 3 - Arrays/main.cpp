8fb825db7809dd36cac487dc5df9c11fbf7cd150
Kelly <700183045@fvtc.edu>
2017-02-28 19:05:13 -0600

    Finished

--- Lab Ex 3 - Arrays/main.cpp
+++ Lab Ex 3 - Arrays/main.cpp
@@ -3,39 +3,45 @@
 // Instructions:
 /*
 	[ ] Write a program that performs the following:
-		[ ] Prompt the user to input five numbers.
-		[ ] Store the input into either an array or a vector.
+		[x] Prompt the user to input five numbers.
+		[x] Store the input into either an array or a vector.
 		[x] Once the user is finished entering numbers:
 			[x] Display the five numbers in reverse order.
-			[x] Display the highest number entered.
-			[x] Display the lowest number entered.
+			[ ] Display the highest number entered.
+			[ ] Display the lowest number entered.
 		[ ] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
 	[ ] Compress(zip) your program and submit it to Blackboard.
 */
 
 #include <iostream>
 #include <conio.h>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
 
 int main()
 {
-	int number;
 	
+	int a;
+	cout << "Enter the amount of numbers you want to display ";
+	cin >> a;
     const int SIZE = 5;
 	int arr[SIZE];
 
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < a; i++)
 	{
-		cout << "Enter your first number? ";
+		cout << "Enter a number? ";
 		cin >> arr[i];
 	}
-	for (int i = SIZE; i > 0 ; i--)
+	for (int i = SIZE - 1; i > -1 ; i--)
 	{
 		cout << arr[i] << endl;
 	}
-	
-
+	std::sort(arr, arr + 5);
+	cout <<"This is the lowest number " << arr[0] << endl;
+	std::sort(arr, arr + 5, std::greater<int>());
+	cout << "This is the highest number " << arr[0];
 	_getch();
 	return 0;
 }
